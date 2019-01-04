ed9e5646061f8b2479ac87bcbf8f17a3d4fdd48b
Evgen Stepanov <evgen.stepanov.911@gmail.com>
2019-01-04 23:28:55 +0300

    Initial Commit

---
+++ test_git/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test_git
+//
+//  Created by Evgeniy A. Stepanov on 04/01/2019.
+//  Copyright © 2019 Evgeniy A. Stepanov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
